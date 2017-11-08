105098464ee9b5a5d87b65fc8ca19037a5f9ade5
Eric Magar <emagar@gmail.com>
2017-11-08 13:00:50 -0600

    Añade mapa df de Chema, sólo falta aclarar incongruencia numérica de sus distritos2018 y los de json del INE

--- mapasComparados/loc/code/coaEx.r
+++ mapasComparados/loc/code/coaEx.r
@@ -73,12 +73,11 @@ setwd(dd)
 ## #
 ## write.csv(d, file = "coaLoc.csv", row.names = FALSE)
 
-## get functions to include population
-source(paste(dd, "code/getPop.r", sep = ""))
-
 ## READ HISTORICAL MAP
 d <- read.csv(file = "coaLoc.csv", stringsAsFactors = FALSE)
 
+## get functions to include population
+source(paste(dd, "code/getPop.r", sep = ""))
 pob05 <- get2005(edon=5)
 pob10 <- get2010(edon=5)
 
