bb606f276c4997759217327c839cc5b12ce62f21
Eric Magar <emagar@gmail.com>
2017-12-20 05:39:52 -0600

    some consolidation to code producing DSI etc in maplocs

--- mapasComparados/loc/code/tous.eric.r
+++ mapasComparados/loc/code/tous.eric.r
@@ -3,6 +3,172 @@ rm(list = ls())
 dd <- "/home/eric/Desktop/MXelsCalendGovt/redistrict/ife.ine/mapasComparados/loc/"
 setwd(dd)
 
+############
+#   1 ags  #
+############
+# Estos son los archivos que circuló humberto
+#ags2 <- read.csv("fuenteAlumnos/agsLoc.csv", stringsAsFactors = FALSE)
+#
+# Estos son los archivos originales
+#ags1 <- read.csv("../../redisProcess/ineRedist2017/deJsonConEtiquetas/loc/agsLoc.csv", stringsAsFactors = FALSE)
+
+## # ags
+## colnames(ags1)
+## ags1 <- ags1[,c("edon","seccion","munn","escenario3")]
+## colnames(ags1) <- c("edon","seccion","munn","disn2016")
+## 
+## # cambia nombres en datos de humberto
+## head(ags2)
+## ags2$s <- NULL
+## ags2$edon <- ags2$munn <- NULL
+## colnames(ags2) <- c("edon","seccion","munn","disn2016b","disn2013") # necesito año de disnold
+## 
+## # fusiona
+## ags <- merge(x = ags1, y = ags2, by = "seccion", all = TRUE)
+## 
+## dim(ags)
+## dim(ags1)
+## dim(ags2)
+## 
+## head(ags)
+## table(ags$disn2016b==ags$disn2016)
+## ags$disn2016b <- NULL
+## 
+## write.csv(ags, file = "fuenteAlumnos/agsLoc.csv", row.names = FALSE) 
+
+
+## # ags
+## ## READ HISTORICAL MAP (MISSING SECCIONES POSSIBLE)
+## d <- read.csv(file = "fuenteAlumnos/agsLoc.csv", stringsAsFactors = FALSE)
+## head(d) 
+## # handy function to rename one data.frame's column
+## rename.col <- function(old=NA, new=NA, what=NA){
+##     old <- old; new <- new; what <- what;
+##     colnames(what)[which(colnames(what)==old)] <- new
+##     return(what)
+## }
+## d <- rename.col(old="disn2013", new="disloc2013", what=d)
+## d <- rename.col(old="disn2016", new="disloc2016", what=d)
+## #
+## # ---> NOTE:                                                                         <--- #
+## # ---> open useEqPrep2fillMissSeccionesLocalMaps.r and run manually to spot errors   <--- #
+## # ---> will generate new eq object with full map (incl. state and federal districts) <--- #
+## 
+## write.csv(eq, file = "agsLoc.csv", row.names = FALSE)
+
+
+## ## prepare dsi
+## ## READ HISTORICAL MAP
+## d <- read.csv(file = "agsLoc.csv", stringsAsFactors = FALSE)
+## head(d)
+## # dsi seen from offspring perspective
+## # new district's "father" and district similarity index, cf. Cox & Katz
+## son    <- d$disloc2016
+## father <- d$disloc2013
+## N <- max(son, na.rm = TRUE)
+## d$father <- NA
+## d$dsi <- 0
+## for (i in 1:N){
+##     #i <- 1 # debug
+##     sel.n <- which(son==i)                  # secciones in new district
+##     tmp <- table(father[sel.n])
+##     target <- as.numeric(names(tmp)[tmp==max(tmp)][1]) # takes first instance in case of tie (dual fathers) 
+##     d$father[sel.n] <- target
+##     sel.f <- which(father==target) # secciones in father district
+##     sel.c <- intersect(sel.n, sel.f)             # secciones common to father and new districts
+##     d$dsi[sel.n] <- round( length(sel.c) / (length(sel.f) + length(sel.n) - length(sel.c)) , 3 )
+## }
+## dsi <- d[duplicated(son)==FALSE,]
+## dsi <- dsi[,c("edon","disloc2016","father","dsi")]
+## dsi <- dsi[order(dsi$dsi),]
+## head(dsi)
+## 
+## write.csv(dsi, file = "simIndex/dist_ags.csv", row.names = FALSE)
+
+
+
+
+
+
+
+############
+#   4 cam  #
+############
+## # Estos son los archivos originales
+## cam1 <- read.csv("../../redisProcess/ineRedist2017/deJsonConEtiquetas/loc/camLoc.csv", stringsAsFactors = FALSE)
+#
+## # campeche
+## head(cam1)
+## cam1 <- cam1[,c("edon","seccion","munn","escenario3")]
+## colnames(cam1) <- c("edon","seccion","munn","disn2018")
+#
+## # añade mapa dibujado en 2010, a mano
+## cam2 <- read.csv("fuenteAlumnos/camMap2010.csv", stringsAsFactors = FALSE)
+## colnames(cam2) <- c("seccion","disn2012")
+## head(cam2)
+#
+## # fusiona siguientes dos columnas
+## cam <- merge(x= cam1, y = cam2, by = "seccion", all = TRUE)
+#
+## dim(cam)
+## dim(cam1)
+## dim(cam2)
+#
+## write.csv(cam, file = "camLoc.csv", row.names = FALSE) # Zabel: usa éste para sacar el insice s de cox y katz
+
+
+## # cam
+## ## READ HISTORICAL MAP (MISSING SECCIONES POSSIBLE)
+## d <- read.csv(file = "fuenteAlumnos/camLoc.csv", stringsAsFactors = FALSE)
+## head(d) # dist_old year needed
+## 
+## # handy function to rename one data.frame's column
+## rename.col <- function(old=NA, new=NA, what=NA){
+##     old <- old; new <- new; what <- what;
+##     colnames(what)[which(colnames(what)==old)] <- new
+##     return(what)
+## }
+## d <- rename.col(old="disn2012", new="disloc2012", what=d)
+## d <- rename.col(old="disn2018", new="disloc2018", what=d)
+## #
+## # ---> NOTE:                                                                         <--- #
+## # ---> open useEqPrep2fillMissSeccionesLocalMaps.r and run manually to spot errors   <--- #
+## # ---> will generate new eq object with full map (incl. state and federal districts) <--- #
+## 
+## write.csv(eq, file = "camLoc.csv", row.names = FALSE)
+
+
+## ## prepare dsi
+## ## READ HISTORICAL MAP
+## d <- read.csv(file = "camLoc.csv", stringsAsFactors = FALSE)
+## head(d)
+## # dsi seen from offspring perspective
+## # new district's "father" and district similarity index, cf. Cox & Katz
+## son    <- d$disloc2018
+## father <- d$disloc2012
+## N <- max(son, na.rm = TRUE)
+## d$father <- NA
+## d$dsi <- 0
+## for (i in 1:N){
+##     #i <- 1 # debug
+##     sel.n <- which(son==i)                  # secciones in new district
+##     tmp <- table(father[sel.n])
+##     target <- as.numeric(names(tmp)[tmp==max(tmp)][1]) # takes first instance in case of tie (dual fathers) 
+##     d$father[sel.n] <- target
+##     sel.f <- which(father==target) # secciones in father district
+##     sel.c <- intersect(sel.n, sel.f)             # secciones common to father and new districts
+##     d$dsi[sel.n] <- round( length(sel.c) / (length(sel.f) + length(sel.n) - length(sel.c)) , 3 )
+## }
+## dsi <- d[duplicated(son)==FALSE,]
+## dsi <- dsi[,c("edon","disloc2018","father","dsi")]
+## head(dsi)
+## dsi <- dsi[order(dsi$dsi),]
+#
+## write.csv(dsi, file = "simIndex/dist_cam.csv", row.names = FALSE)
+
+
+
+
 
 ############
 #   9  df  #
