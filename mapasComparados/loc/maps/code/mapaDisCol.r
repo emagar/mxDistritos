10f274f61e75caee3923b11d5751644c12868a74
Eric Magar <emagar@gmail.com>
2018-01-08 03:08:44 -0600

    Fix width height of colLoc pngs

--- mapasComparados/loc/maps/code/mapaDisCol.r
+++ mapasComparados/loc/maps/code/mapaDisCol.r
@@ -508,12 +508,12 @@ p84 <- function(x = NA){
 
 portray <- se.map$bastion  # elegir qué reportará el mapa 2
 portray2 <- se.map$ncascol # elegir qué reportará el mapa 3
-dn <- 1                  # elegir un distrito
-for (dn in 1:16){
-    print(paste("disn =", dn))
-# plot state map with highlighted district
-png(file = paste(md2, edo, dn, "-1.png", sep = ""))
-par(mar=c(2,2,2,1)) ## SETS B L U R MARGIN SIZES
+dn <- 16                  # elegir un distrito
+## for (dn in 1:16){
+##     print(paste("disn =", dn))
+## # plot state map with highlighted district
+png(file = paste(md2, edo, dn, "-1.png", sep = ""), width=10, height=10, units="cm", res=144) 
+par(mar=c(0,0,2,0)) ## SETS B L U R MARGIN SIZES
 plot(shave(p84(ed.map$col), p = .16), col = "white", axes = TRUE, main = "Colima (mapa local 2018)")#, bg = "lightblue")
 plot(p84(ed.map$jal), col = "white", add = TRUE, lty = 3)
 plot(p84(ed.map$mic), col = "white", add = TRUE, lty = 3)
@@ -550,7 +550,8 @@ xx <- .12*max(b$max[2] - b$min[2], b$max[1] - b$min[1])
 bg.os <- openmap(c(b$max[2]+xx,b$min[1]-xx), c(b$min[2]-xx,b$max[1]+xx), type=c("osm"))
 bg <- bg.os
 #
-png(file = paste(md2, edo, dn, "-2.png", sep = ""), width=400, height=400, res=40) # see http://blog.revolutionanalytics.com/2009/01/10-tips-for-making-your-r-graphics-look-their-best.html
+
+png(file = paste(md2, edo, dn, "-2.png", sep = ""), width=15, height=15, units="cm", res=144) 
 par(mar=c(0,0,2,0)) ## SETS B L U R MARGIN SIZES
 tmp <-  dl.map$cab[which(dl.map$disloc==dn)]
 tmp2 <- dl.map$dsi[which(dl.map$disloc==dn)]
@@ -608,7 +609,7 @@ library(prettymapr)
 addnortharrow(pos = ifelse(lp[dn]=="topright", "topleft", "topright"), scale=.75)
 addscalebar(style = "ticks", pos = ifelse(lp[dn]=="bottomright", "bottomleft", "bottomright"))
 dev.off()
-}
+#}
 
 # plot same distrito's nCasillas
 loc <- c("topright","bottomleft","topleft","bottomleft","bottomleft",
