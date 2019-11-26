cc9191dd4bb6cb1ae5539b784de36a116786427d
Eric Magar <emagar@gmail.com>
2019-11-26 12:01:02 -0600

    En route to get 2013 party proposals

--- code/get2013proposals.r
+++ code/get2013proposals.r
@@ -2,16 +2,102 @@ rm(list = ls())
                     
 path <- "/home/eric/Desktop/MXelsCalendGovt/redistrict/ife.ine/redisProcess/ifeRedist2013/"
 
+########################################################
+## Function to process all party proposals in state e ##
+########################################################
+get.pty.props <- function(estado = NA){
+    #edon <- 2  #debug
+    edon <- estado
+    edo <- c("ags", "bc", "bcs", "cam", "coa", "col", "cps", "cua", "df", "dgo", "gua", "gue", "hgo", "jal", "mex", "mic", "mor", "nay", "nl", "oax", "pue", "que", "qui", "san", "sin", "son", "tab", "tam", "tla", "ver", "yuc", "zac")[edon]
+    #dd1   <- paste(path, "1erEscenario2013/", edo, sep = "")
+    dd1pp <- paste(path, "1erEscenarioComentariosPartidos/", edo, "/Escenarios/", sep = "")
+    #dd2   <- paste(path, "2oEscenario/", edo, sep = "")
+    #dd2pp <- paste(path, "2oEscenarioComentariosPartidos/", edo, sep = "")
+    #dd3   <- paste(path, "3erEscenario/",      sep = "")
+    setwd(dd1pp)
+    #
+    # explore directory
+    files.tmp <- list.files()
+    sel <- grep("tmp", files.tmp)
+    if (length(sel)>0) files.tmp <- files.tmp[-sel] # drop tmp from list if present
+    #
+    # select/subset edon rows in dat for manipulation
+    sel.dat <- which(dat$edon==edon)
+    dat.tmp <- dat[sel.dat,] # subset dat rows to manipulate
+    #
+    for (i in 1:length(files.tmp)){ # loop over available party proposals
+        #i <- 4 # debug
+        sel.file <- files.tmp[i];
+        ptyn <- sub(pattern = "[A-Za-z_áéíóú]+_[0-9]{1}_([0-9]+)_[0-9]+.zip", replacement = "\\1", sel.file)
+        #
+        # determine party authoring the proposal
+        if (ptyn=="19")  next # pty <- "e"; # DIRECCION DE CARTOGRAFIA (DCE) --- original escenario
+        if (ptyn=="2")   next # pty <- "eloc.ignore"; # CLV CARGADO POR EL PRESIDENTE DE LA MISMA
+        if (ptyn=="32")  pty <- "pan.clv"; # PAN ANTE LA CLV
+        if (ptyn=="33")  pty <- "pri.clv"; # PRI ANTE LA CLV
+        if (ptyn=="34")  pty <- "prd.clv"; # PRD ANTE LA CLV
+        if (ptyn=="35")  pty <- "pt.clv"; # PT ANTE LA CLV
+        if (ptyn=="36")  pty <- "pvem.clv"; # PVEM ANTE LA CLV
+        if (ptyn=="37")  pty <- "mc.clv"; # MC ANTE LA CLV
+        if (ptyn=="38")  pty <- "pna.clv"; # NA ANTE LA CLV
+        if (ptyn=="42")  pty <- "pan.cnv"; # PAN ANTE LA CNV
+        if (ptyn=="43")  pty <- "pri.cnv"; # PRI ANTE LA CNV
+        if (ptyn=="44")  pty <- "prd.cnv"; # PRD ANTE LA CNV
+        if (ptyn=="45")  pty <- "pt.cnv"; # PT ANTE LA CNV
+        if (ptyn=="46")  pty <- "pvem.cnv"; # PVEM ANTE LA CNV
+        if (ptyn=="47")  pty <- "mc.cnv"; # MC ANTE LA CNV
+        if (ptyn=="48")  pty <- "pna.cnv"; # NA ANTE LA CNV
+        #
+        # unzip relevant data into tmp/ folder
+        unzip(sel.file, exdir = "tmp")
+        #name.col <- unzip(sel.file, list = TRUE)$Name # pick unzipped file's name for dataframe's column
+        #
+        # read data 
+        tmp <- read.csv(file = "tmp/escenario.dat.ife", sep = "\t", header = FALSE)
+        colnames(tmp) <- c("edon","prop","munn","seccion");
+        #colnames(tmp)[2] <- paste(pty, "1", sep="");
+        #
+        tmp  <- within(tmp, ife <- edon*1000 + munn) # add ife municipio code
+        #head(tmp); # debug
+        #
+        # column to manipulate
+        target.col <- grep(pty, colnames(dat.tmp))
+        #
+        # handle seccion==0 cases, ie. full municipios 
+        sel.zero <- which(tmp$seccion==0) # select obs where district is reported for munn only
+        if (length(sel.zero)>0) {
+            for (m in sel.zero){
+                #m <- sel.zero[2] # debug
+                sel.m <- which(dat.tmp$ife==tmp$ife[m])   # indices with same municipio number as seccion==0 cases
+                dat.tmp[sel.m,target.col] <- tmp$prop[m]  # plug district number to those indices
+            }
+        }
+        #
+        # handle seccion!=0 cases
+        sel.zero <- which(tmp$seccion!=0) # select obs where seccion assigned 
+        if (length(sel.zero)>0) {
+            for (s in sel.zero){
+                #s <- sel.zero[9] # debug
+                sel.m <- which(dat.tmp$seccion==tmp$seccion[s])    # indices with same municipio number as seccion==0 cases
+                dat.tmp[sel.m,target.col] <- tmp$prop[s]  # plug district number to those indices
+            }
+        }
+    }
+    # return manipulated data
+    dat[sel.dat,] <- dat.tmp
+    ## tmp2 <- dat.tmp[,target.col]
+    ## table(tmp2==dat.tmp[,target.col], useNA = "always")
+    return(dat)
+}
+
 # get master secciones file to pour new data in
 eq <- read.csv("/home/eric/Desktop/MXelsCalendGovt/redistrict/ife.ine/equivSecc/tablaEquivalenciasSeccionalesDesde1994.csv")
 sel <- which(colnames(eq) %in% c("ord","edo","mun","edosecn","dis1994","dis1997","dis2000","dis2003","dis2006","dis2009","dis2012","dis2015","dis2018","OBSERVACIONES","action","fr.to","orig.dest","when","color","coment"))
 eq <- eq[,-sel]
-eq[1,]
 colnames(eq)[which(colnames(eq)=="dis2013")] <- "e3"
 #
 # rename
 dat <- eq
-
 
 #################
 ## escenario 1 ##
@@ -23,10 +109,10 @@ for (e in 1:32){
     edon <- e # 1:32
     edo <- c("ags", "bc", "bcs", "cam", "coa", "col", "cps", "cua", "df", "dgo", "gua", "gue", "hgo", "jal", "mex", "mic", "mor", "nay", "nl", "oax", "pue", "que", "qui", "san", "sin", "son", "tab", "tam", "tla", "ver", "yuc", "zac")[edon]
     dd1   <- paste(path, "1erEscenario2013/", edo, sep = "")
-    dd1pp <- paste(path, "1erEscenarioComentariosPartidos/", edo, sep = "")
-    dd2   <- paste(path, "2oEscenario/", edo, sep = "")
-    dd2pp <- paste(path, "2oEscenarioComentariosPartidos/", edo, sep = "")
-    dd3   <- paste(path, "3erEscenario/",      sep = "")
+    ## dd1pp <- paste(path, "1erEscenarioComentariosPartidos/", edo, sep = "")
+    ## dd2   <- paste(path, "2oEscenario/", edo, sep = "")
+    ## dd2pp <- paste(path, "2oEscenarioComentariosPartidos/", edo, sep = "")
+    ## dd3   <- paste(path, "3erEscenario/",      sep = "")
     #
     setwd(dd1)
     #
@@ -47,260 +133,36 @@ for (s in 1:length(sel)){
     sel.m <- which(dat$ife==e1$ife[sel[s]]) # indices with same municipio number as seccion==0 cases
     dat$e1[sel.m] <- e1$e1[sel[s]]          # plug district number to those indices
 }
+rm(e,e1,sel,sel.m,s,tmp,edo,edon)
 
 #################################
 ## escenario 1 party proposals ##
 #################################
-e1 <- data.frame() # will receive data
+# universe of party proposals in 2013
+props <- c("pan.clv", "pri.clv", "prd.clv", "pt.clv", "pvem.clv", "mc.clv", "pna.clv", "pan.cnv", "pri.cnv", "prd.cnv", "pt.cnv", "pvem.cnv", "mc.cnv", "pna.cnv")
 #
+# ADD COLUMNS TO dat WITH ABOVE NAMES AND e1 INFO
+tmp <- matrix(dat$e1, length(dat$e1), length(props)) # repeat e1 as many times as there are proposing parties
+tmp <- as.data.frame(tmp)
+colnames(tmp) <- paste(props,1,sep="") # rename proposal columns
+dat <- cbind(dat, tmp) # paste to data
+
+dat <- get.pty.props(estado=2)
+
 for (e in 1:32){
-    #e <- 1
-    edon <- e # 1:32
-    edo <- c("ags", "bc", "bcs", "cam", "coa", "col", "cps", "cua", "df", "dgo", "gua", "gue", "hgo", "jal", "mex", "mic", "mor", "nay", "nl", "oax", "pue", "que", "qui", "san", "sin", "son", "tab", "tam", "tla", "ver", "yuc", "zac")[edon]
-    dd1   <- paste(path, "1erEscenario2013/", edo, sep = "")
-    dd1pp <- paste(path, "1erEscenarioComentariosPartidos/", edo, "/Escenarios/", sep = "")
-    dd2   <- paste(path, "2oEscenario/", edo, sep = "")
-    dd2pp <- paste(path, "2oEscenarioComentariosPartidos/", edo, sep = "")
-    dd3   <- paste(path, "3erEscenario/",      sep = "")
-    #
-    setwd(dd1pp)
-    #
-    # explore workdir
-    files.tmp <- list.files()
-    files.tmp <- files.tmp[grep(paste(edo, "[Fed|Loc].+zip", sep = ""), files.tmp)]     # keep only zipped in listing
-    tmp <- grep("19", files.tmp)                    # locate first scenario...
-    files.tmp <- files.tmp[-tmp]                    # and drop it (already added)
-    files.tmp
-    #
-    i <- 1
-    ## # file contents
-    ## unzip(tmp, list = TRUE)
-    unzip(files.tmp[i])
-    name.tmp <- unzip(files.tmp[i], list = TRUE)$Name # pick unzipped file's name for dataframe's column
-
-    tmp <- read.csv(file = "escenario.dat.ife", sep = "\t", header = FALSE)
-    colnames(tmp) <- c("edon","e1","munn","seccion")
-    #
-    e1 <- rbind(e1, tmp)
+    message(sprintf("loop %s of %s", e, 32))
+    #e <- 32 # 1:32
+    dat <- get.pty.props(estado=e)
 }
-#
-e1  <- within(e1, ife <- edon*1000 + munn) # add ife municipio code
-#
-# handle seccion!=0 cases
-sel <- which(e1$seccion==0) # select obs where district is reported for munn only
-dat <- merge(x = dat, y = e1[-sel,c("edon","seccion","e1")], by = c("edon","seccion"), all = TRUE)
-# handle seccion==0 cases, ie. full municipios 
-for (s in 1:length(sel)){
-    #s <- 1 # debug
-    sel.m <- which(dat$ife==e1$ife[sel[s]]) # indices with same municipio number as seccion==0 cases
-    dat$e1[sel.m] <- e1$e1[sel[s]]          # plug district number to those indices
-}
+warnings()
 
-
-
-
-dat[1:12,]
-dim(dat)
-
-
-# handle full municipios 
-sel <- which(output$seccion==0) # select obs where district is reported for munn only
-    for (s in 1:length(sel)){
-        #s <- 2 # debug
-        sel.m <- which(output$munn==output$munn[s])
-        output$dis62.20.157[sel.m] <- output$dis62.20.157[s]
-        output$dis62.20.37 [sel.m] <- output$dis62.20.37 [s]
-        output$dis62.20.39 [sel.m] <- output$dis62.20.39 [s]
-        output$dis62.20.38 [sel.m] <- output$dis62.20.38 [s]
-        output$dis62.20.32 [sel.m] <- output$dis62.20.32 [s]
-        output$dis62.20.41 [sel.m] <- output$dis62.20.41 [s]
-        output$dis62.20.33 [sel.m] <- output$dis62.20.33 [s]
-        output$dis62.20.35 [sel.m] <- output$dis62.20.35 [s]
-        output$dis62.20.36 [sel.m] <- output$dis62.20.36 [s]
-        output$dis71.20.157[sel.m] <- output$dis71.20.157[s]
-    }
-}
-
-
-
-dir()
-# explore workdir
-files.tmp <- list.files()
-files.tmp <- files.tmp[grep(paste(edo, "[Fed|Loc].+zip", sep = ""), files.tmp)]     # keep only zipped in listing
-files.tmp <- files.tmp[-grep("geojson", files.tmp)] # drop geojson
-tmp <- grep("100|157", files.tmp)                   # locate first/second scenario... 
-files.tmp <- c(files.tmp[tmp], files.tmp[-tmp])     # and place it at start of loop
-files.tmp
-
-i <- 1
-## # file contents
-## unzip(tmp, list = TRUE)
-unzip(files.tmp[i])
-name.tmp <- unzip(files.tmp[i], list = TRUE)$Name # pick unzipped file's name for dataframe's column
-# extract secciones
-dat.tmp <- getSec(edon = edon, jsonFile = name.tmp)
-# simplify name
-name.tmp <- sub(pattern = "SICED--D[FL]--(.+)--.+--.+", replacement = "\\1", name.tmp)
-name.tmp <- gsub(pattern = "[-]", replacement = ".", name.tmp)
-colnames(dat.tmp)[which(colnames(dat.tmp)=="dis")] <- paste("dis", name.tmp, sep = "") # change name accordingly
-#
-output <- dat.tmp
-#
-for (i in 2:length(files.tmp)){
-    #i <- 2 # debug
-    unzip(files.tmp[i])
-    name.tmp <- unzip(files.tmp[i], list = TRUE)$Name # pick unzipped file's name
-    # extract secciones
-    dat.tmp <- getSec(edon = edon, jsonFile = name.tmp)
-    # simplify name
-    name.tmp <- sub(pattern = "SICED--D[FL]--(.+)--.+--.+", replacement = "\\1", name.tmp)
-    name.tmp <- gsub(pattern = "[-]", replacement = ".", name.tmp)
-    colnames(dat.tmp)[which(colnames(dat.tmp)=="dis")] <- paste("dis", name.tmp, sep = "") # change name accordingly
-    #
-    output <- merge(x = output, y = dat.tmp, by = c("edon", "seccion", "munn"), all = TRUE) # merge data
-}
-head(output)
-
-e1 <- output
-
-# 2do escenario
-setwd(dd2) 
-# explore workdir
-files.tmp <- list.files()
-files.tmp <- files.tmp[grep(paste(edo, "[Fed|Loc].+zip", sep = ""), files.tmp)]     # keep only zipped in listing
-files.tmp <- files.tmp[-grep("geojson", files.tmp)] # drop geojson
-tmp <- grep("100|157", files.tmp)                   # locate first/second scenario... 
-files.tmp <- c(files.tmp[tmp], files.tmp[-tmp])     # and place it at start of loop
-files.tmp
-
-i <- 1
-## # file contents
-## unzip(tmp, list = TRUE)
-unzip(files.tmp[i])
-name.tmp <- unzip(files.tmp[i], list = TRUE)$Name # pick unzipped file's name for dataframe's column
-# extract secciones
-dat.tmp <- getSec(edon = edon, jsonFile = name.tmp)
-# simplify name
-name.tmp <- sub(pattern = "SICED--D[FL]--(.+)--.+--.+", replacement = "\\1", name.tmp)
-name.tmp <- gsub(pattern = "[-]", replacement = ".", name.tmp)
-colnames(dat.tmp)[which(colnames(dat.tmp)=="dis")] <- paste("dis", name.tmp, sep = "") # change name accordingly
-#
-output <- dat.tmp
-#
-for (i in 2:length(files.tmp)){
-    #i <- 2 # debug
-    unzip(files.tmp[i])
-    name.tmp <- unzip(files.tmp[i], list = TRUE)$Name # pick unzipped file's name
-    # extract secciones
-    dat.tmp <- getSec(edon = edon, jsonFile = name.tmp)
-    # simplify name
-    name.tmp <- sub(pattern = "SICED--D[FL]--(.+)--.+--.+", replacement = "\\1", name.tmp)
-    name.tmp <- gsub(pattern = "[-]", replacement = ".", name.tmp)
-    colnames(dat.tmp)[which(colnames(dat.tmp)=="dis")] <- paste("dis", name.tmp, sep = "") # change name accordingly
-    #
-    output <- merge(x = output, y = dat.tmp, by = c("edon", "seccion", "munn"), all = TRUE) # merge data
-}
-head(output)
-
-output <- merge(x = e1, y = output, by = c("edon","seccion","munn"), all = TRUE)
-e1 <- output
-
-# 3er escenario
-setwd(dd3) 
-# explore workdir
-files.tmp <- list.files()
-files.tmp <- files.tmp[grep(paste(edo, "[Fed|Loc].+zip", sep = ""), files.tmp)]     # keep only zipped in listing
-files.tmp <- files.tmp[-grep("geojson", files.tmp)] # drop geojson
-tmp <- grep("100|157", files.tmp)                   # locate first/second scenario... 
-files.tmp <- c(files.tmp[tmp], files.tmp[-tmp])     # and place it at start of loop
-files.tmp
-
-i <- 1
-## # file contents
-## unzip(tmp, list = TRUE)
-unzip(files.tmp[i])
-name.tmp <- unzip(files.tmp[i], list = TRUE)$Name # pick unzipped file's name for dataframe's column
-# extract secciones
-dat.tmp <- getSec(edon = edon, jsonFile = name.tmp)
-# simplify name
-name.tmp <- sub(pattern = "SICED--D[FL]--(.+)--.+--.+", replacement = "\\1", name.tmp)
-name.tmp <- gsub(pattern = "[-]", replacement = ".", name.tmp)
-colnames(dat.tmp)[which(colnames(dat.tmp)=="dis")] <- paste("dis", name.tmp, sep = "") # change name accordingly
-#
-output <- dat.tmp
-#
-## for (i in 2:length(files.tmp)){
-##     #i <- 2 # debug
-##     unzip(files.tmp[i])
-##     name.tmp <- unzip(files.tmp[i], list = TRUE)$Name # pick unzipped file's name
-##     # extract secciones
-##     dat.tmp <- getSec(edon = edon, jsonFile = name.tmp)
-##     # simplify name
-##     name.tmp <- sub(pattern = "SICED--D[FL]--(.+)--.+--.+", replacement = "\\1", name.tmp)
-##     name.tmp <- gsub(pattern = "[-]", replacement = ".", name.tmp)
-##     colnames(dat.tmp)[which(colnames(dat.tmp)=="dis")] <- paste("dis", name.tmp, sep = "") # change name accordingly
-##     #
-##     output <- merge(x = output, y = dat.tmp, by = c("edon", "seccion", "munn"), all = TRUE) # merge data
-## }
-head(output)
-#colnames(output) <- c("edon","seccion","munn","dis71.20.157cg","dis71.20.157")
-
-output <- merge(x = e1, y = output, by = c("edon","seccion","munn"), all = TRUE)
-
+dat[1,]
+table(dat$e1==dat$pan.cnv1)
 
 write.csv(output, file = paste(path, edo, "Fed.csv", sep = ""), row.names = FALSE); rm(e1)
 
 
 
-# function extracting secciones and corresponding districts in given json file
-getSec <- function(edo=NA, edon=NA, jsonFile=NA){
-    # check that input is correctly specified
-    output <- data.frame() # will receive secciones to plug into eqMp
-    edos <- c("ags", "bc", "bcs", "cam", "coa", "col", "cps", "cua", "df", "dgo", "gua", "gue", "hgo", "jal", "mex", "mic", "mor", "nay", "nl", "oax", "pue", "que", "qui", "san", "sin", "son", "tab", "tam", "tla", "ver", "yuc", "zac")
-    if (is.na(edo)==TRUE & is.na(edon)==TRUE){
-        stop("Must provide edo or edon.")
-    }
-    if (is.na(edo)==FALSE & (is.character(edo)==FALSE | length(intersect(edo, edos))==0)){
-        stop("edo must be one of valid abbreviations: ags, bc, bcs, cam, coa, col, cps, cua, df, dgo, gua, gue, hgo, jal, mex, mic, mor, nay, nl, oax, pue, que, qui, san, sin, son, tab, tam, tla, ver, yuc, zac")
-    }
-    if (is.na(edon)==FALSE & (is.numeric(edon)==FALSE | edon<1 | edon>32)){
-        stop("edon must be numeric in 1:32.")
-    }
-    if (is.na(edo)==FALSE & is.na(edon)==FALSE){
-        if (edos[edon]!=edo) stop("edo--edon mismatch.")
-    }
-    if (is.na(edo)==TRUE){
-        edo <- edos[edon]
-    }
-    if (is.na(edon)==TRUE){
-        edon <- which(edos==edo)
-    }
-    # read file
-    txt <- readLines(con = jsonFile, warn = FALSE)
-    #txt <- file.path(dd, "OaxacaLocal.json") 
-    dat <- fromJSON(txt)#, flatten=TRUE)
-    D <- max(dat$distritos$d) # number of districts in state
-    for (dis in 1:D){
-        #dis <- 1 # debug
-        tmp.m <- dat$distritos$des[[dis]]$mu
-        M <- length(tmp.m)
-        if (M==1){
-            tmp.s <- dat$distritos$des[[dis]]$s[[1]]
-            S <- length(tmp.s)
-            tmp.e <- data.frame(edon=rep(edon,S), seccion=tmp.s, munn=rep(tmp.m,S), dis=rep(dis,S))
-        } else {
-            tmp.e <- data.frame()
-            for(munn in 1:M){
-                #munn <- 2 # debug
-                tmp.s <- dat$distritos$des[[dis]]$s[[munn]]
-                S <- length(tmp.s)
-                tmp.e <- rbind(tmp.e, data.frame(edon=rep(edon,S), seccion=tmp.s, munn=rep(tmp.m[munn],S), dis=rep(dis,S)))
-            }
-        }
-        output <- rbind(output, tmp.e)
-        }
-    return(output)
-}
 
 
 
--- code/tmp.r
+++ code/tmp.r
@@ -1,48 +1,85 @@
-props <- c("pan.clv", "pri.clv", "prd.clv", "pt.clv", "pvem.clv", "mc.clv", "pna.clv", "pan.cnv", "pri.cnv", "prd.cnv", "pt.cnv", "pvem.cnv", "mc.cnv", "pna.cnv")
-
-get.pty <- function(pty=NA){
-    if (pty=="pan.clv")  ptyn <- 32; # PAN ANTE LA CLV
-    if (pty=="pri.clv")  ptyn <- 33; # PRI ANTE LA CLV
-    if (pty=="prd.clv")  ptyn <- 34; # PRD ANTE LA CLV
-    if (pty=="pt.clv")   ptyn <- 35; # PT ANTE LA CLV
-    if (pty=="pvem.clv") ptyn <- 36; # PVEM ANTE LA CLV
-    if (pty=="mc.clv")   ptyn <- 37; # MC ANTE LA CLV
-    if (pty=="pna.clv")  ptyn <- 38; # NA ANTE LA CLV
-    if (pty=="pan.cnv")  ptyn <- 42; # PAN ANTE LA CNV
-    if (pty=="pri.cnv")  ptyn <- 43; # PRI ANTE LA CNV
-    if (pty=="prd.cnv")  ptyn <- 44; # PRD ANTE LA CNV
-    if (pty=="pt.cnv")   ptyn <- 45; # PT ANTE LA CNV
-    if (pty=="pvem.cnv") ptyn <- 46; # PVEM ANTE LA CNV
-    if (pty=="mc.cnv")   ptyn <- 47; # MC ANTE LA CNV
-    if (pty=="pna.cnv")  ptyn <- 48; # NA ANTE LA CNV
+############################################################
+## Function to process all party proposals in given state ##
+############################################################
+get.pty.props <- function(edon==NA){
+    #edon <- 1  #debug
+    edo <- c("ags", "bc", "bcs", "cam", "coa", "col", "cps", "cua", "df", "dgo", "gua", "gue", "hgo", "jal", "mex", "mic", "mor", "nay", "nl", "oax", "pue", "que", "qui", "san", "sin", "son", "tab", "tam", "tla", "ver", "yuc", "zac")[edon]
+    #dd1   <- paste(path, "1erEscenario2013/", edo, sep = "")
+    dd1pp <- paste(path, "1erEscenarioComentariosPartidos/", edo, "/Escenarios/", sep = "")
+    #dd2   <- paste(path, "2oEscenario/", edo, sep = "")
+    #dd2pp <- paste(path, "2oEscenarioComentariosPartidos/", edo, sep = "")
+    #dd3   <- paste(path, "3erEscenario/",      sep = "")
+    setwd(dd1pp)
     #
     files.tmp <- list.files()
-    sel.file <- grep(paste(ptyn,"[_]", sep = ""), files.tmp)
-    if (length(sel.file)==0){
-        print(paste("No proposal by", pty));
-        stop
+    sel <- grep("tmp", files.tmp)
+    if (length(sel)>0) files.tmp <- files.tmp[-sel] # drop tmp from list if present
+    for (i in 1:length(files.tmp)){ # loop over available party proposals
+        #i <- 3 # debug
+        sel.file <- files.tmp[i];
+        ptyn <- sub(pattern = "[A-Za-z]+_[0-9]{1}_([0-9]+)_[0-9]+.zip", replacement = "\\1", sel.file)
+        #
+        # determine party authoring the proposal
+        if (ptyn=="32")  pty <- "pan.clv"; # PAN ANTE LA CLV
+        if (ptyn=="33")  pty <- "pri.clv"; # PRI ANTE LA CLV
+        if (ptyn=="34")  pty <- "prd.clv"; # PRD ANTE LA CLV
+        if (ptyn=="35")  pty <- "pt.clv"; # PT ANTE LA CLV
+        if (ptyn=="36")  pty <- "pvem.clv"; # PVEM ANTE LA CLV
+        if (ptyn=="37")  pty <- "mc.clv"; # MC ANTE LA CLV
+        if (ptyn=="38")  pty <- "pna.clv"; # NA ANTE LA CLV
+        if (ptyn=="42")  pty <- "pan.cnv"; # PAN ANTE LA CNV
+        if (ptyn=="43")  pty <- "pri.cnv"; # PRI ANTE LA CNV
+        if (ptyn=="44")  pty <- "prd.cnv"; # PRD ANTE LA CNV
+        if (ptyn=="45")  pty <- "pt.cnv"; # PT ANTE LA CNV
+        if (ptyn=="46")  pty <- "pvem.cnv"; # PVEM ANTE LA CNV
+        if (ptyn=="47")  pty <- "mc.cnv"; # MC ANTE LA CNV
+        if (ptyn=="48")  pty <- "pna.cnv"; # NA ANTE LA CNV
+        if (ptyn=="19")  {warning("Original escenario ignored"); break} # pty <- "e"; # DIRECCION DE CARTOGRAFIA (DCE) --- original escenario
+        if (ptyn=="2")   {warning("Cargado x clv is ignored"); break} # pty <- "eloc.ignore"; # CLV CARGADO POR EL PRESIDENTE DE LA MISMA
+        #
+        # unzip relevant data into tmp/ folder
+        unzip(sel.file, exdir = "tmp")
+        #name.col <- unzip(sel.file, list = TRUE)$Name # pick unzipped file's name for dataframe's column
+        #
+        # read data 
+        tmp <- read.csv(file = "tmp/escenario.dat.ife", sep = "\t", header = FALSE)
+        colnames(tmp) <- c("edon","prop","munn","seccion");
+        #colnames(tmp)[2] <- paste(pty, "1", sep="");
+        #head(tmp); # debug
+        # now need to either export tmp to be added later or (better) to plug it into dat
+        # deal with seccion==0 first
+        # then with rest
+        #
+        # select/subset edon rows in dat
+        sel.dat <- which(dat$edon==edon)
+        dat.tmp <- dat[sel.dat,] # subset dat rows to manipulate
+        target.col <- grep(pty, colnames(dat.tmp)) # column to manipulate
+        #
+        # handle seccion==0 cases, ie. full municipios 
+        sel.zero <- which(tmp$seccion==0) # select obs where district is reported for munn only
+        if (length(sel.zero)>0) {
+            for (m in 1:length(sel.tmp)){
+                #m <- 9 # debug
+                case <- sel.zero[m];
+                sel.m <- which(dat.tmp$ife==e1$ife[case])    # indices with same municipio number as seccion==0 cases
+                dat.tmp[sel.m,target.col] <- tmp$prop[case]  # plug district number to those indices
+            }
+        }
+        #
+        # handle seccion!=0 cases
+        sel.zero <- which(tmp$seccion!=0) # select obs where seccion assigned 
+        if (length(sel.zero)>0) {
+            for (s in sel.tmp){
+                #s <- sel.tmp[9] # debug
+                sel.m <- which(dat.tmp$seccion==tmp$seccion[s])    # indices with same municipio number as seccion==0 cases
+                dat.tmp[sel.m,target.col] <- tmp$prop[s]  # plug district number to those indices
+            }
+        }
+        #
+        # return manipulated data
+        dat[sel.dat,] <- dat.tmp
+        ## tmp2 <- dat.tmp[,target.col]
+        ## table(tmp2==dat.tmp[,target.col], useNA = "always")
     }
-    unzip(files.tmp[sel.file], exdir = "tmp")
-    name.tmp <- unzip(files.tmp[sel.file], list = TRUE)$Name # pick unzipped file's name for dataframe's column
-    #
-    tmp <- read.csv(file = "tmp/escenario.dat.ife", sep = "\t", header = FALSE)
-    colnames(tmp) <- c("edon","no.name","munn","seccion");
-    colnames(tmp)[2] <- paste(pty, "1", sep="");
-    #
-    return(tmp)
 }
 
-for (i in length(props)){
-    #i <- 14 # debug
-    tmp <- "nihil";                # clear name
-    tmp <- get.pty(pty=props[i]);  # use function to read party
-    if (tmp=="nihil") next;
-    assign(props[i], tmp);         # rename info with pty name
-}
-
-pri.clv[1,]
-
-ls()
-
-    
-    
