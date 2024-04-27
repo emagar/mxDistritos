141b52f383785149acb540ab4c989c7f88d1b41e
Eric Magar <emagar@gmail.com>
2024-04-27 13:11:40 -0600

    adds reverse 18-24 dsi

--- mapasComparados/fed/code/dsi-for-2024-map.r
+++ mapasComparados/fed/code/dsi-for-2024-map.r
@@ -22,10 +22,11 @@ table(is.na(es$dis2018),is.na(es$dis2024))
 ## ######################################################### ##
 ###############################################################
 ##
-###################
-## 2021 = father ##
-## 2024 = son    ##
-###################
+################################
+## son's similarity to father ##
+## 2021 = father              ##
+## 2024 = son                 ##
+################################
 dsi <- data.frame()
 for (e in 1:32){ # loop over states
     sel <- which(es$edon==e)
@@ -61,14 +62,56 @@ for (e in 1:32){ # loop over states
     dsi <- rbind(dsi, d)
 }
 
+################################
+## father's similarity to son ##
+## 2021 = father              ##
+## 2024 = son                 ##
+################################
+dsi2 <- data.frame()
+for (e in 1:32){ # loop over states
+    ##e <- 1
+    sel <- which(es$edon==e)
+    d <- es[sel,]
+    father <- d$dis2018 # 2021 used 2018 map and has most reseccionamiento updates
+    son <- as.numeric(d$dis2024)
+    sel.drop <- which(son==0) # quick fix for re-seccionamiento, should have no impact because new secciones map districts too
+    ##d$seccion[sel.drop]
+    if (length(sel.drop)>0){
+        d <- d[-sel.drop,]; father <- father[-sel.drop]; son <- son[-sel.drop]
+    }
+    father <- father - e*100
+    son    <- son    - e*100
+    N <- max(father, na.rm = TRUE)
+    d$dis <- father
+    d$son <- NA
+    d$dsi <- 0
+    for (i in 1:N){ # loop over districts
+        #i <- 3 # debug
+        sel.n <- which(father==i)               # secciones in old district (father)
+        tmp <- table(son[sel.n])                # how many secciones did each potnetial father contribute to son
+        target <- as.numeric(names(tmp)[tmp==max(tmp)][1]) # picks largest as father (first in case of tie) 
+        d$son[sel.n] <- target
+        sel.f <- which(son==target)             # secciones in son district
+        sel.c <- intersect(sel.n, sel.f)        # secciones common to father and son
+        d$dsi[sel.n] <- round( length(sel.c) / (length(sel.f) + length(sel.n) - length(sel.c)) , 3 )
+    }
+    d <- d[duplicated(father)==FALSE,]
+    d <- d[,c("edon","dis","son","dsi")]
+    d <- d[order(d$dis),]
+    d$dis    <- d$dis    + e*100
+    d$father <- d$son + e*100
+    dsi2 <- rbind(dsi2, d)
+}
+
+
 #dsi <- dsi[-which(dsi$dis==0),] # will drop secciones that are missing... dealing w reseccionamiento above will make this unnecessary
 #
-rm(e,i,d,N,dsi,sel,sel.n,sel.f,sel.c,target,tmp,father,son,sel.drop)
 
 
 
 # export dsis
 setwd(dd)
 dir()
-write.csv(dsi, file = "dsi24v18.csv", row.names = FALSE)
+write.csv(dsi,  file = "dsi24v18.csv",    row.names = FALSE)
+write.csv(dsi2, file = "dsi24v18rev.csv", row.names = FALSE)
 
